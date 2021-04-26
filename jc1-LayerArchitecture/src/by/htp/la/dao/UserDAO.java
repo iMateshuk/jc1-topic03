62d335f6902f09f71f26697b400fe96858fba27c
iMateshuk <80713398+iMateshuk@users.noreply.github.com>
2021-04-26 18:51:27 +0300

    Delete jc1-LayerArchitecture/src/by/htp/la directory

--- jc1-LayerArchitecture/src/by/htp/la/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package by.htp.la.dao;
-
-import by.htp.la.bean.User;
-
-public interface UserDAO {
-	
-	void signIn (String login, String password);
-	void registrationUser(User user);
-}
--- jc1-LayerArchitecture/src/by/htp/la/service/LibraryService.java
+++
@@ -1,9 +0,0 @@
-package by.htp.la.service;
-
-import by.htp.la.bean.Book;
-
-public interface LibraryService {
-
-	void addNewBook(Book book);
-	void addEditedBook(Book book);
-}
